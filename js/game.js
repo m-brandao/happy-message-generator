01c38886de934a45b06090c9b21a5ac1e4ecc61e
m-brandao <miguel.brandao@feedzai.com>
2023-01-11 16:13:26 +0000

    adding start screen

--- js/game.js
+++ js/game.js
@@ -1,11 +1,17 @@
 function startGame(){
     // transition to set hight to 35vh between 0s and 2s 
-    document.getElementById("game-header").style.minHeight = '35vh';
+    document.getElementById("game-header").style.minHeight = '300px';
 
     //  putting opacity on the button and removing it after the animation
     const startGameButton = document.getElementById("start-game");
     startGameButton.style.opacity = '0';
+
+    // get the div with the game
+    const gameScreen = document.querySelector('#game-screen');
+    gameScreen.classList.remove("uk-hidden");
+    
+    // execute with delay
     setTimeout(function () {
-        startGameButton.parentElement.remove();
+        startGameButton.remove();
     }, 2500);
 }
